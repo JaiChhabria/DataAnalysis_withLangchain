e14daa81585faa7f0445744ab763f54bd13908fb
JaiChhabria <31588714+JaiChhabria@users.noreply.github.com>
2023-11-07 11:02:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ from datetime import timedelta
 def query_agent(df, query):
     openai_api_key = st.secrets["OPENAI_API_KEY"]
     llm = OpenAI(openai_api_key=openai_api_key)
-    agent = create_pandas_dataframe_agent(llm, df, verbose=True)
+    agent = pd.DataFrame(llm,df,verbose-True)
+    #agent = create_pandas_dataframe_agent(llm, df, verbose=True)
     return agent.run(query)
 
 # Function to plot a bar chart using Plotly
